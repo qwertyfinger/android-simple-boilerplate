82923ccdb169be8fd6b83cb15c71820cab071ecb
Andrii Chubko <chubkoandriy@gmail.com>
2018-11-07 19:44:51 +0200

    Remove tests dependent on build type

--- app/src/test/java/com/qwertyfinger/androidsimpleboilerplate/ExampleRobolectricTest.kt
+++ app/src/test/java/com/qwertyfinger/androidsimpleboilerplate/ExampleRobolectricTest.kt
@@ -24,15 +24,12 @@
 
 package com.qwertyfinger.androidsimpleboilerplate
 
-import android.app.Application
 import androidx.test.core.app.ActivityScenario
-import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.junit.runner.RunWith
 
@@ -43,13 +40,6 @@ import org.junit.runner.RunWith
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleRobolectricTest {
-  @Test
-  fun useAppContext() {
-    // Context of the app under test.
-    val appContext = ApplicationProvider.getApplicationContext<Application>()
-    assertEquals("com.qwertyfinger.androidsimpleboilerplate.debug", appContext.packageName)
-  }
-
   @Test
   fun checkHelloWorldLabel() {
     ActivityScenario.launch(MainActivity::class.java)
