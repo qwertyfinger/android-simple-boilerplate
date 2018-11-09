002250befb53683e1bb3f02cd3111120227db3b5
Andrii Chubko <chubkoandriy@gmail.com>
2018-11-09 17:55:10 +0200

    Add comment explaining the reason for RxAndroidInitializer

--- app/src/main/java/com/qwertyfinger/androidsimpleboilerplate/appinitializer/RxAndroidInitializer.kt
+++ app/src/main/java/com/qwertyfinger/androidsimpleboilerplate/appinitializer/RxAndroidInitializer.kt
@@ -6,6 +6,8 @@ import io.reactivex.android.plugins.RxAndroidPlugins
 import io.reactivex.android.schedulers.AndroidSchedulers
 import javax.inject.Inject
 
+// Used to improve UI performance when using AndroidSchedulers.
+// See https://medium.com/@sweers/rxandroids-new-async-api-4ab5b3ad3e93
 class RxAndroidInitializer @Inject constructor() : AppInitializer {
     override fun init(application: Application) {
         RxAndroidPlugins.setInitMainThreadSchedulerHandler {
