c38b2b90f33dd77a5726b769594c424bbb4a5f41
Andrii Chubko <chubkoandriy@gmail.com>
2018-11-22 23:28:53 +0200

    Fix spacing for Dependencies.kt

--- buildSrc/src/main/java/buildsrc/Dependencies.kt
+++ buildSrc/src/main/java/buildsrc/Dependencies.kt
@@ -105,12 +105,12 @@ object Libraries {
   }
 
   object Gradle {
-    val android =         "com.android.tools.build:gradle:${Versions.Gradle.android}"
-    val dexcount =        "com.getkeepsafe.dexcount:dexcount-gradle-plugin:${Versions.Gradle.dexcount}"
-    val fabric =          "io.fabric.tools:gradle:${Versions.Gradle.fabric}"
+    val android =          "com.android.tools.build:gradle:${Versions.Gradle.android}"
+    val dexcount =         "com.getkeepsafe.dexcount:dexcount-gradle-plugin:${Versions.Gradle.dexcount}"
+    val fabric =           "io.fabric.tools:gradle:${Versions.Gradle.fabric}"
     val firebase_plugins = "com.google.firebase:firebase-plugins:${Versions.Gradle.firebase_plugins}"
-    val google_services = "com.google.gms:google-services:${Versions.Gradle.google_services}"
-    val kotlin =          "org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlin}"
+    val google_services =  "com.google.gms:google-services:${Versions.Gradle.google_services}"
+    val kotlin =           "org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlin}"
   }
 
   val coroutines =         "org.jetbrains.kotlinx:kotlinx-coroutines-core:${Versions.coroutines}"
