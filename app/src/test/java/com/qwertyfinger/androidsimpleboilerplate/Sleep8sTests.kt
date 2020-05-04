f638d522a9ee8b9a45f6e954463c4951cf67066b
Andrii Chubko <andrii.chubko@uptech.team>
2020-05-04 13:05:41 +0300

    Add more tests

---
+++ app/src/test/java/com/qwertyfinger/androidsimpleboilerplate/Sleep8sTests.kt
@@ -0,0 +1,54 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2020 Andrii Chubko
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.qwertyfinger.androidsimpleboilerplate
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class Sleep8sTests {
+  @Test
+  fun `sleep for 8s`() {
+    Thread.sleep(8000)
+    assert(true)
+  }
+
+  @Test
+  fun `sleep for 4s`() {
+    Thread.sleep(4000)
+    assert(true)
+  }
+
+  @Test
+  fun `sleep for 2s`() {
+    Thread.sleep(2000)
+    assert(true)
+  }
+
+  @Test
+  fun `sleep for 3s`() {
+    Thread.sleep(3000)
+    assert(true)
+  }
+}
